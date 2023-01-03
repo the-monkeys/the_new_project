17a26de06988e958c62a096b0e484091ecf8c32e
Dave Augustus <davetweetlive@gmail.com>
2023-01-04 00:02:41 +0530

    Setting up projects
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- gen.sh
+++ gen.sh
@@ -3,4 +3,9 @@
 # Delete all the .pb.go files from all the directories.
 
 # Generate code from proto files
-protoc pkg/**/pb/*.proto --go_out=. --go-grpc_out=.
+# protoc pkg/**/pb/*.proto --go_out=. --go-grpc_out=.
+
+for files in $(ls -d */)
+do
+    protoc $files pkg/**/pb/*.proto --go_out=. --go-grpc_out=.
+done
