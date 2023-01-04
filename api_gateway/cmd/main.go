16caf7d9b46bfd8a2393ecdba7cce5501869dacd
Dave Augustus <davetweetlive@gmail.com>
2023-01-05 00:12:01 +0530

    gateway setup
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- api_gateway/cmd/main.go
+++ api_gateway/cmd/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"github.com/89minutes/the_new_project/api_gateway/config"
+	"github.com/89minutes/the_new_project/api_gateway/pkg/auth"
+	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	logrus.Info("starting the api gateway...")
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		logrus.Fatalf("failed to load the config: %v", err)
+	}
+
+	router := gin.Default()
+
+	auth.RegisterRouter(router, &cfg)
+
+	router.Run(cfg.Port)
 }
---
+++ api_gateway/pkg/auth/routes.go
@@ -0,0 +1,96 @@
+package auth
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/89minutes/the_new_project/api_gateway/config"
+	"github.com/89minutes/the_new_project/api_gateway/pkg/auth/pb"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+)
+
+type LoginRequestBody struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type RegisterRequestBody struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type serviceClient struct {
+	Client pb.AuthServiceClient
+}
+
+func InitServiceClient(cfg *config.Config) pb.AuthServiceClient {
+	cc, err := grpc.Dial(cfg.AuthSvcUrl, grpc.WithInsecure())
+	if err != nil {
+		logrus.Errorf("cannot dial to grpc auth server: %v", err)
+	}
+
+	return pb.NewAuthServiceClient(cc)
+}
+
+func RegisterRouter(router *gin.Engine, cfg *config.Config) *serviceClient {
+	asc := &serviceClient{
+		Client: InitServiceClient(cfg),
+	}
+	routes := router.Group("/api/v1/auth")
+	routes.POST("/register", asc.Register)
+	routes.POST("/login", asc.Login)
+
+	return asc
+}
+
+func (asc *serviceClient) Register(ctx *gin.Context) {
+	Register(ctx, asc.Client)
+}
+
+func (asc *serviceClient) Login(ctx *gin.Context) {
+	Login(ctx, asc.Client)
+}
+
+func Register(ctx *gin.Context, c pb.AuthServiceClient) {
+	body := RegisterRequestBody{}
+
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	res, err := c.Register(context.Background(), &pb.RegisterRequest{
+		Email:    body.Email,
+		Password: body.Password,
+	})
+
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadGateway, err)
+		return
+	}
+
+	ctx.JSON(int(res.Status), &res)
+}
+
+func Login(ctx *gin.Context, c pb.AuthServiceClient) {
+	b := LoginRequestBody{}
+
+	if err := ctx.BindJSON(&b); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	res, err := c.Login(context.Background(), &pb.LoginRequest{
+		Email:    b.Email,
+		Password: b.Password,
+	})
+
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadGateway, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, &res)
+}
