ec0097219d187eca3476a0810b8b617565c6232d
Dave Augustus <36985548+daveaugustus@users.noreply.github.com>
2023-01-24 19:16:03 +0530

    Migrate added (#10)
    * Migrate added
    
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>
    
    * post test commits
    
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>
    
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- Makefile
+++ Makefile
@@ -5,3 +5,6 @@ proto:
 	protoc services/user_profile/user_service/pb/*.proto --go_out=. --go-grpc_out=.
 	protoc services/blogsandposts_service/blog_service/pb/*.proto --go_out=. --go-grpc_out=.
 
+
+migrate-up:
+	migrate -path db/user/migration -database "postgresql://root:Secret@localhost:5432/the_monkeys?sslmode=disable" -verbose up
---
+++ db/user/migration/000001_init_user_schema.up.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS the_monkneys_user
+(
+    id SERIAL,
+    unique_id text NOT NULL,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    email text NOT NULL,
+    password text NOT NULL,
+    profile_pic bytea,
+    create_time text ,
+    update_time text ,
+    is_active boolean,
+    role integer,
+    last_login text,
+    country_code text,
+    mobile_no text ,
+    about text,
+    instagram text,
+    twitter text,
+
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS pw_reset
+(
+    id SERIAL ,
+    user_id bigint NOT NULL,
+    email text ,
+    recovery_hash text ,
+    time_out timestamp with time zone,
+    last_password_reset timestamp with time zone,
+
+    CONSTRAINT password_resets_pkey PRIMARY KEY (id),
+
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id)
+        REFERENCES the_monkneys_user(id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION 
+);
+
