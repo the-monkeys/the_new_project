7a884e08e1d586d9504f1555b17d2e128d21ff21
Irak Rigia <tarakrigia@gmail.com>
2023-02-05 20:38:22 +0530

    FEAT: Added an uninstallation script that removes all the_monkey related files and services from the system.

---
+++ scripts/uninstall.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+# This script uninstalls everything related to THE_MONKEYS.
+
+source ${MONKEY_SCRIPTS}/common.sh
+
+CERTS_PATH="${MONKEY_ROOT}/vault/certs"
+CSR_CONFIG_FILE="${CERTS_PATH}/csr.conf"
+CERT_CONFIG_FILE="${CERTS_PATH}/cert.conf"
+PRIVATE_KEY_FILE="${CERTS_PATH}/prv_key.pem"
+CSR_FILE="${CERTS_PATH}/csr.pem"
+ROOT_CA_FILE="${CERTS_PATH}/root_ca.pem"
+ROOT_CA_KEY_FILE="${CERTS_PATH}/root_ca_key.pem"
+CERT_FILE="${CERTS_PATH}/cert.pem"
+
+function uninstallCerts()
+{
+    rm -rf "$CERTS_PATH"
+}
+uninstallCerts
+
+function uninstallService()
+{
+    # change these variables to match your setup
+    # TODO: SERVICE_NAMEs are all the folder names under services/
+    SERVICE_NAME="$1"
+    SERVICE_WORKDIR="${MONKEY_ROOT}/bin"
+    SERVICE_EXEC="${SERVICE_WORKDIR}/${SERVICE_NAME}"
+
+    # start the service
+    systemctl stop $SERVICE_NAME
+
+    # enable the service to start on boot
+    systemctl disable $SERVICE_NAME
+
+    # reload systemd manager configuration
+    systemctl daemon-reload
+
+    echo "rm -f /etc/systemd/system/$SERVICE_NAME.service"
+    rm -f /etc/systemd/system/$SERVICE_NAME.service
+
+    echo "rm -f $SERVICE_EXEC"
+    rm -f "$SERVICE_EXEC"
+}
+
+THE_MONKEYS_SERVICES=(
+    "api_gateway"
+    "article_and_post"
+    "auth_service"
+    "blogsandposts_service"
+    "the_monkey_pg"
+    "user_profile"
+)
+
+echo "[Installing THE_MONKEYS services...]"
+for s in "${THE_MONKEYS_SERVICES[@]}"
+do
+    echo "Uninstalling service: ${s}"
+    uninstallService "$s"
+done
+
+echo "Deleting MONKEY_ROOT"
+rm -rf "$MONKEY_ROOT"
+
+echo "Uninstall successful."
