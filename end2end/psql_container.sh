c648f4a06b3489847933f910787ff48ca43b412f
Dave Augustus <davetweetlive@gmail.com>
2023-02-12 04:16:18 +0530

    Added postgres docker image script
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- end2end/psql_container.sh
+++ end2end/psql_container.sh
@@ -19,12 +19,41 @@ CONTAINER_NAME=subtle_art
 IMAGE_NAME=postgres
 
 # Set the Postgres password
+POSTGRES_USER = postgres
 POSTGRES_PASSWORD=Secret
 POSTGRES_DB=the_monkeys
 
 # Create and run the container
 docker run -d --name $CONTAINER_NAME \
-  -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
-  -e POSTGRES_DB=$POSTGRES_DB \
-  -p 5432:5432 \
-  $IMAGE_NAME
+    -e POSTGRES_USER=$POSTGRES_USER
+    -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+    -e POSTGRES_DB=$POSTGRES_DB \
+    -p 5432:5432 \
+    $IMAGE_NAME
+
+
+MIGRATION_DIR=psql/migration
+
+sql_files=$(ls $MIGRATION_DIR/*.sql)
+echo $sql_files
+
+# Loop through each SQL file and migrate it to the database
+for file in $sql_files
+do
+  echo "Migrating $file..."
+
+  # Use the docker exec command to run psql in the container and execute the SQL file
+  docker exec -i $CONTAINER_NAME psql -U $POSTGRES_USER -d $DATABASE_NAME -v ON_ERROR_STOP=1 -f $file
+
+  # Check the exit code of psql and exit the script if there was an error
+  if [ $? -ne 0 ]
+  then
+    echo "Error migrating $file"
+    exit 1
+  fi
+done
+
+echo "All migrations completed successfully."
+
+
+
