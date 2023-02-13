efb4657b65130caa3ad12a103787143c22ff4d96
Irak Rigia <tarakrigia@gmail.com>
2023-02-13 18:39:33 +0530

    TEMP3

--- services/api_gateway/cmd/main.go
+++ services/api_gateway/cmd/main.go
@@ -87,11 +87,11 @@ func (s *Server) start(ctx context.Context) {
 	if os.Getenv("NO_TLS") != "1" {
 		tlsCert = os.Getenv("TLS_CERT")
 		if tlsCert == "" {
-			tlsCert = "certs/cert.pem"
+			tlsCert = "/the_monkeys/vault/certs/cert.pem"
 		}
 		tlsKey = os.Getenv("TLS_KEY")
 		if tlsKey == "" {
-			tlsKey = "certs/key.pem"
+			tlsKey = "/the_monkeys/vault/certs/key.pem"
 		}
 	}
 
