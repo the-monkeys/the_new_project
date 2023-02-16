41b1900c58809b980cb4455c10c5b8c4ed35672e
Dave Augustus <davetweetlive@gmail.com>
2023-02-17 00:59:58 +0530

    Some changes
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+
+for dir in services/*/cmd; 
+
+do 
+    # Split dir to get the service name
+    IFS='/'
+    read -ra ADDR <<</$dir
+    microservice_name=${ADDR[2]}
+
+    # Merge the dir again to change dir to the cmd
+    IFS=' '
+    read -ra ADDR <<<$microservice_name
+
+    (cd "$dir" && go build -o "/usr/local/bin/the_monkeys/$microservice_name"); 
+done
+
+
+
+# restart services to load the new code changes
